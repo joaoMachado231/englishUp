c6669a23017d1b6ed30d36e00dc6932a190144a6
João Pedro Bruno Machado <joao.machado0601@gmail.com>
2026-09-16 01:34:41 -0300

    feat: Implementing hero layout

---
+++ src/app/components/hero/hero.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { WppButton } from '../../shared/wpp-button/wpp-button';
+
+@Component({
+  imports: [WppButton],
+  selector: 'app-hero',
+  styleUrl: './hero.component.scss',
+  templateUrl: './hero.component.html',
+})
+export class HeroComponent {}
