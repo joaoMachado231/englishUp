d1c8a5b6f2bba48854ecc5b9cd997d7ca5a40607
João Pedro Bruno Machado <joao.machado0601@gmail.com>
2026-09-22 00:13:33 -0300

    fix: inserting corret icons on course section

--- src/app/app.ts
+++ src/app/app.ts
@@ -41,22 +41,22 @@ export class App {
 
   public readonly courseCards: CardModel[] = [
     {
-      iconSrc: 'assets/icons/graduation-icon.svg',
+      iconSrc: 'assets/icons/globe-icon.svg',
       title: 'Inglês Geral',
       description: 'Do básico ao avançado para o dia a dia.'
     },
     {
-      iconSrc: 'assets/icons/methodology-icon.svg',
+      iconSrc: 'assets/icons/plane-icon.svg',
       title: 'Inglês para viagens',
       description: 'Aprenda o essencial para viajar com segurança.'
     },
     {
-      iconSrc: 'assets/icons/group-icon.svg',
+      iconSrc: 'assets/icons/briefcase-business-icon.svg',
       title: 'Inglês para Negócios',
       description: 'Comunicação profissional para sua carreira.'
     },
     {
-      iconSrc: 'assets/icons/chart-icon.svg',
+      iconSrc: 'assets/icons/book-open-check-icon.svg',
       title: 'Preparatório Exames Internacionais',
       description: 'Prepare-se para os principais exames como TOEFL, IELTS e mais.'
     },
